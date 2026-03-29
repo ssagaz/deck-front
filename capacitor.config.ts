3e8e21c308c0ab0443523aad0a092b71b5e32ff2
Patrick <patrickmarques.job@hotmail.com>
2026-03-29 07:22:07 -0300

    fix appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'deck.app',
+  appId: 'com.xl.MyXL.Giant-staging',
   appName: 'Deck',
   webDir: 'dist/deckesfihas/browser'
 };
